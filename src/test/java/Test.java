43c251dad28f0123b0c08a08ff7200d0c888f080
Degus <pd2405@yandex.ru>
2019-02-05 22:58:09 +0300

    first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,35 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Test {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/home/degu/Downloads/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.dns-shop.ru/");
+    }
+
+    @org.junit.Test
+    public void test() {
+       driver.findElement(By.linkText("Компьютеры и периферия")).click();
+       driver.findElement(By.partialLinkText("Компьютерные системы")).click();
+       driver.findElement(By.partialLinkText("Системные блоки")).click();
+       driver.findElement(By.xpath(".//*[text()=' По возрастанию цены ']/..")).click();
+       driver.findElement(By.xpath("//a[@data-id='2']/..")).click();
+       driver.findElement(By.xpath("//h3")).click();
+
+
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
